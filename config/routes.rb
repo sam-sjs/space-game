4bc6d47c07e4622737e4ab4724bff34f8d654120
Sam Skinner <sam.sjskinner@gmail.com>
2020-12-04 22:56:16 +1100

    Added seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-
+  root to: 'pages#home'
+  resources :users
+  resources :systems
+  resources :planets
+  resources :ships
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,80 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+print "Creating Users..."
+
+User.destroy_all
+
+User.create!(
+  name: 'Sam',
+  email: 'sam@sam.com',
+  password_digest: 'chicken'
+)
+
+User.create!(
+  name: 'Sean',
+  email: 'sean@sean.com',
+  password_digest: 'chicken'
+)
+
+puts "created #{User.count} users."
+
+
+print "Creating Systems..."
+
+System.destroy_all
+
+System.create!(
+  name: 'Alpha',
+  planets: 4
+)
+
+puts "created #{System.count} users."
+
+
+print "Creating Planets..."
+
+Planet.destroy_all
+
+Planet.create!(
+  name: "Planet 1",
+  size: 3,
+  fuel_present: true,
+  fuel_constructed: false,
+  image: 'http://placekitten.com/300/300'
+)
+
+Planet.create!(
+  name: "Planet 2",
+  size: 2,
+  fuel_present: false,
+  fuel_constructed: true,
+  image: 'http://placekitten.com/200/200'
+)
+
+Planet.create!(
+  name: "Planet 3",
+  size: 1,
+  fuel_present: false,
+  fuel_constructed: false,
+  image: 'http://placekitten.com/100/100'
+)
+
+Planet.create!(
+  name: "Planet 4",
+  size: 2,
+  fuel_present: false,
+  fuel_constructed: false,
+  image: 'http://placekitten.com/200/200'
+)
+
+puts "created #{Planet.count} planets."
+
+
+print "Creating Ships..."
+
+Ship.destroy_all
+
+Ship.create!(
+  name: 'MyShip!',
+  fuel: 10
+)
+
+puts "created #{Ship.count} ships."

77c566b4bd641f2224322cab0006798becdac24f
Sam Skinner <sam.sjskinner@gmail.com>
2020-12-04 22:00:52 +1100

    recreated controllers because what are plurals

---
+++ app/controllers/ships_controller.rb
@@ -0,0 +1,22 @@
+class ShipsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end

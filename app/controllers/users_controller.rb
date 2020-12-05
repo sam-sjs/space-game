dff534d73c2620cfeb9af4013800f1db0b24a366
Sam Skinner <sam.sjskinner@gmail.com>
2020-12-05 23:41:36 +1100

    can now create new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create user_params
+
+    if @user.persisted?
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def index
@@ -19,4 +28,11 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :systems
   has_many :planets, through: :systems
   has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :name, length: {minimum: 2}
 end
